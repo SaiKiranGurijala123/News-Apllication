30cad9d6f7d4200825ee7d051a0122db565861c8
SaiKiranGurijala123 <147523360+SaiKiranGurijala123@users.noreply.github.com>
2025-03-06 06:16:57 +0530

    updated1

--- Script.js
+++ Script.js
@@ -97,7 +97,8 @@ window.location.href = "news.html";
     //   alert("method called");
     // }
 // for displaying the two bot basesd on the URL
-     document.addEventListener("load", function() {
+     document.addEventListener('load', function() {
+      alert("loadded")
       
       var currentURL = window.location.href;
        console.log(currentURL + " this is the current URL");
